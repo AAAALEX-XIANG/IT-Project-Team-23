62824a8d6f91e43270bd83f6f457eced19b037ca
AAAALEX-XIANG <aaaalex@foxmail.com>
2020-08-26 17:44:33 +1000

    Code to learn

---
+++ 1/springboot-backend/src/main/resources/public/precache-manifest.2cfc7dfee5702b7ca035a409866b4f15.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e99c7c4c8dca9c8c60ddcae882d0ec5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eefe394d6d6c221ad9ef",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "a22c1cb03b47176498b6",
+    "url": "/static/css/main.850209ea.chunk.css"
+  },
+  {
+    "revision": "eefe394d6d6c221ad9ef",
+    "url": "/static/js/2.decf18dd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.decf18dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a22c1cb03b47176498b6",
+    "url": "/static/js/main.fb2b286a.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
