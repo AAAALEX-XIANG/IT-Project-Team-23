bbb703ab01bb3b247ee0bcc2b2ef0f95b515756f
AAAALEX-XIANG <aaaalex@foxmail.com>
2020-10-22 09:58:43 +1100

    minor fix

---
+++ backend/target/classes/static/precache-manifest.fdb4c7fb75ae01c6c0a73abdbf2ee09e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d17af703a1e88623e50c1aa58fed31cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f4c8429e33566879712",
+    "url": "/static/css/2.faf6d629.chunk.css"
+  },
+  {
+    "revision": "e8c53734cf8c52488729",
+    "url": "/static/css/main.a55fdccb.chunk.css"
+  },
+  {
+    "revision": "2f4c8429e33566879712",
+    "url": "/static/js/2.aba8c461.chunk.js"
+  },
+  {
+    "revision": "97b083be2e05f6c86d4aab0d88d43a94",
+    "url": "/static/js/2.aba8c461.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8c53734cf8c52488729",
+    "url": "/static/js/main.df9c6f8b.chunk.js"
+  },
+  {
+    "revision": "ce56515975c5bc5da7be",
+    "url": "/static/js/runtime-main.79246033.js"
+  },
+  {
+    "revision": "8d3e0a894cd5a30b5e4d9199752756ab",
+    "url": "/static/media/empty.8d3e0a89.png"
+  },
+  {
+    "revision": "0889b514f08f8da9cfada37670c8b9ab",
+    "url": "/static/media/sky.0889b514.jpeg"
+  }
+]);
